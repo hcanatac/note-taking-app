5ed8e611cfa153faa0214f0a36401d082ec199fa
hcanatac <can35mm@gmail.com>
2022-11-10 17:07:54 +0300

    added deleting function

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import {useState, createContext} from 'react'
 import noteData from './noteData.js'
 
 
-
 export const dataHolder = createContext()
 
 
@@ -13,22 +12,26 @@ export default function App() {
   const [data, setData] = useState(noteData)
 
   function setActive(idFromClick) {
-
     const newArray = []
-    
     data.map(a=> {
-      if(a.id === idFromClick) {a.active= !a.active;newArray.push(a)} else {a.active=false;newArray.push(a)}
+      if(a.id === idFromClick) {a.active= true;newArray.push(a)} else {a.active=false;newArray.push(a)}
     })
-
     setData(newArray)
-    
+  }
+
+  function deleteNote(idFromClick) {
+    const newArray = []
+    data.map(a=> {
+        if(a.id!==idFromClick) {newArray.push(a)}
+    })
+    setData(newArray)
     console.log(newArray)
-    console.log(data)
+    console.log(data) //Why data is not updating?
   }
 
   return (
     <div className='stage'>
-      <dataHolder.Provider value={setActive}>
+      <dataHolder.Provider value={[setActive, deleteNote]}>
         <Menu value={data}/>
         <Note value={data}/>
       </dataHolder.Provider>
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,25 +2,25 @@ import React from 'react'
 import Add from './Add'
 import { FiTrash } from 'react-icons/fi'
 import { dataHolder } from '../App'
-import { useContext, useEffect } from 'react'
+import { useContext } from 'react'
 
 
 
 export default function Menu(data) {
     const dataGoingBack = useContext(dataHolder)
-    const setActive = dataGoingBack
+    const setActive = dataGoingBack[0]
+    const deleteNote = dataGoingBack[1]
 
 
     function changeActive(e) {
         const dataId = e.target.id
         setActive(dataId);
-        console.log(data.value) //Check if updated data coming back from props or not?
     }
 
-
-    useEffect(()=>{
-        console.log("useEffect runs")
-    },[data.value])
+    function deleteIt(e) {
+        const dataId = e.target.parentElement.id
+        deleteNote(dataId);
+    }
 
 
     function getHeaders(){
@@ -28,7 +28,7 @@ export default function Menu(data) {
             data.value.map(a=>(
                 <div id={a.id} key={Math.floor(Math.random() * 999999)} className='noteEntry' style={a.active?{backgroundColor:'#68756E'}:{backgroundColor:'#4D5F55'}} onClick={changeActive}>
                     <div id={a.id}>{a.topic.length>55?a.topic.substring(0,55)+"...":a.topic}</div>
-                    <div className='deleteEntry'><FiTrash/></div>
+                    <div className='deleteEntry' onClick={deleteIt}><FiTrash/></div>
                 </div>
                 )
             )
